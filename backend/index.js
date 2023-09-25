b8b44401ab38aa646c2a95d21219719109e1ce29
Kaastriot <105289225+Kaastriot@users.noreply.github.com>
2023-09-25 12:35:15 +0200

    new

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,14 @@ app.use('/api/users', userRoutes);
 app.use('/api/movies', moviesRoutes);
 app.use('/api/contacts', contactRoutes);
 
-mongoose.connect(process.env.DB_CONNECTION_URL);
+mongoose.connect(process.env.DB_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('DB Connection Successfull')
+}).catch(err => {
+    console.log('Error connecting DB: ', err.message);
+});
 
 app.use("/images", express.static("images"));
 
